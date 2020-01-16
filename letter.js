d4bcb1a05d0463944c2c10eff63229e50ad7c79d
annammattis <54290779+annammattis@users.noreply.github.com>
2020-01-16 09:43:07 -0500

    letter and word update

--- letter.js
+++ letter.js
@@ -2,21 +2,21 @@
 
 var character = "";
 
-function Letter(character, guessed) {
+function Letter(character) {
     // A string value to store the underlying character for the letter
     this.character = character;
     // A boolean value that stores whether that letter has been guessed yet
     this.guessed = false;
     // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-    this.display = function() {
+    this.display = function () {
         if (this.guessed === true) {
             return this.character;
         } else {
             return "_";
         }
-    } 
+    }
     // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-    this.characterCheckup = function(userCharacter) {
+    this.characterCheckup = function (userCharacter) {
         if (this.character === userCharacter) {
             this.guessed = true;
         } else {
@@ -24,7 +24,7 @@ function Letter(character, guessed) {
         }
     }
 }
- 
+
 module.exports = Letter();
 
 
--- word.js
+++ word.js
@@ -1,19 +1,34 @@
 var Letter = require("./letter.js");
 
 // Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
-function Word(letters) {
+function Word(answer) {
     // An array of new Letter objects representing the letters of the underlying word
-    this.letters = this.wordGuess(letters);
-    // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-    this.wordGuess = function(word) {
-        var letterArray = [];
-        var wordArray = word.split();
-        for (i = 0, i < letterArray.push(new letters(letter[i]), i++);
-    }
-    // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
-    this.lettersCheckup = function() {
+    this.letterArray = [];
 
+    for (var i = 0; i < answer.length; i++) {
+        var letter = new Letter(answer[i]);
+        this.letterArray.push(letter);
+        // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
+        this.output = function () {
+            var answerOutput = "";
+            for (var i = 0; i < this.letterArray.length; i++) {
+                answerOutput += this.letterArray[i] + " ";
+            }
+
+            console.log(answerOutput + "\n===========================\n");
+        }
+
+        // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+        this.wordGuess = function (word) {
+            // var wordArray = word.split();
+            for (var i = 0; i < this.letterArray.length; i++) {
+                this.letterArray[i].guess.split(word);
+            }
+
+        }
     }
+
+
 }
 
 
